31c51c9e64f4f152c08de0a442328031f33218a9
Parvez Ahmed <miazyparvez@gmail.com>
2021-09-05 18:42:57 +0600

    Show data from localstorage

--- src/pages/Employees/Employees.js
+++ src/pages/Employees/Employees.js
@@ -1,9 +1,10 @@
-import React from "react";
-import EmployeeForm from "./EmployeeForm";
+import React,{useState} from "react";
+//import EmployeeForm from "./EmployeeForm";
 import PageHeader from "../../App/components/PageHeader";
 import PeopleOutlineTwoToneIcon from "@material-ui/icons/PeopleOutlineTwoTone";
-import { makeStyles, Paper } from "@material-ui/core";
-
+import { makeStyles, Paper, Table, TableBody, TableCell, TableRow } from "@material-ui/core";
+import useTable from "../../App/components/useTable";
+import * as employeeService from "../../services/employeeService";
 const useStyles = makeStyles((theme) => ({
   pageContent: {
     margin: theme.spacing(5),
@@ -12,8 +13,10 @@ const useStyles = makeStyles((theme) => ({
 }));
 export default function Employees() {
   const classes = useStyles();
+  const [records, setRecords] = useState(employeeService.getAllEmployees())
+  const {TblContainer} = useTable();
   return (
-    <div>
+    <>
       <PageHeader
         title="New Employee"
         subtitle="Form Design With Validation"
@@ -21,8 +24,37 @@ export default function Employees() {
       />
 
       <Paper className={classes.pageContent}>
-        <EmployeeForm />
+        {/* <EmployeeForm /> */}
+        <TblContainer>
+          <TableBody>
+            {
+              records.map(
+                item=>(
+                  (
+                    <TableRow key={item.id}>
+                    <TableCell>
+                      {
+                        item.fullName
+                      }
+                    </TableCell>
+                    <TableCell>
+                      {
+                        item.email
+                      }
+                    </TableCell>
+                    <TableCell>
+                      {
+                        item.mobile
+                      }
+                    </TableCell>
+                  </TableRow>
+                  )
+                )
+              )
+            }
+          </TableBody>
+        </TblContainer>
       </Paper>
-    </div>
+    </>
   );
 }
