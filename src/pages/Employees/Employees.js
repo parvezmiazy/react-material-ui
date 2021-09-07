06559ced2006854f6ca2fd183f23471f15d7fcdb
Parvez Ahmed <miazyparvez@gmail.com>
2021-09-08 01:35:52 +0600

    bug fixed

--- src/pages/Employees/Employees.js
+++ src/pages/Employees/Employees.js
@@ -1,8 +1,15 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 //import EmployeeForm from "./EmployeeForm";
 import PageHeader from "../../App/components/PageHeader";
 import PeopleOutlineTwoToneIcon from "@material-ui/icons/PeopleOutlineTwoTone";
-import { makeStyles, Paper, Table, TableBody, TableCell, TableRow } from "@material-ui/core";
+import {
+  makeStyles,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+} from "@material-ui/core";
 import useTable from "../../App/components/useTable";
 import * as employeeService from "../../services/employeeService";
 const useStyles = makeStyles((theme) => ({
@@ -13,8 +20,8 @@ const useStyles = makeStyles((theme) => ({
 }));
 export default function Employees() {
   const classes = useStyles();
-  const [records, setRecords] = useState(employeeService.getAllEmployees())
-  const {TblContainer} = useTable();
+  const [records, setRecords] = useState(employeeService.getAllEmployees());
+  const { TblContainer } = useTable();
   return (
     <>
       <PageHeader
@@ -27,31 +34,13 @@ export default function Employees() {
         {/* <EmployeeForm /> */}
         <TblContainer>
           <TableBody>
-            {
-              records.map(
-                item=>(
-                  (
-                    <TableRow key={item.id}>
-                    <TableCell>
-                      {
-                        item.fullName
-                      }
-                    </TableCell>
-                    <TableCell>
-                      {
-                        item.email
-                      }
-                    </TableCell>
-                    <TableCell>
-                      {
-                        item.mobile
-                      }
-                    </TableCell>
-                  </TableRow>
-                  )
-                )
-              )
-            }
+            {records.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.fullName}</TableCell>
+                <TableCell>{item.email}</TableCell>
+                <TableCell>{item.mobile}</TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </TblContainer>
       </Paper>
